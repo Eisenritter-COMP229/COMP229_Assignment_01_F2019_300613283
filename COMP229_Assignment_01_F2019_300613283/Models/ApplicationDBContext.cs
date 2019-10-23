ea825239414cc5a6a6fc95655558375414e2e647
George Zhou <georgezhou89@hotmail.com>
2019-10-23 00:09:20 -0400

    Configured Database

---
+++ COMP229_Assignment_01_F2019_300613283/Models/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+// This Class allows this to connect with the database
+namespace COMP229_Assignment_01_F2019_300613283.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        // Constructor
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+        // Dataset of Recipes, get the recipes from database.
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
---
+++ COMP229_Assignment_01_F2019_300613283/Models/EFRecipeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP229_Assignment_01_F2019_300613283.Models
+{
+    public class EFRecipeRepository:IRecipeRepository
+    {
+        private ApplicationDBContext context;
+
+        // Whenever EFRecipeRepository is created sets the context to the context
+        public EFRecipeRepository(ApplicationDBContext ctx)
+        {
+            context = ctx;
+        }
+        // Return set of recipes from database
+        public IQueryable<Recipe> Recipes => context.Recipes;
+    }
+}
